4291148eb64b94c89220a7b2093a4b66f315d9cb
aron <vifus1@gmail.com>
2021-11-07 17:11:05 +0100

    contact page

--- components/Contact/Contact.vue
+++ components/Contact/Contact.vue
@@ -19,6 +19,7 @@ pl:
   <div>
     <Breadcrumb title="Kontakt" />
     <section class="contact-page section-b-space">
+      <Modal v-if="showModal" :message="mMessage" xtitle="xxxxx444xxxx"/>
       <div class="container">
         <div class="row section-b-space">
           <div class="col-lg-7 map">
@@ -80,7 +81,7 @@ pl:
                 <div class="col-md-6">
                   <label for="firstName">{{ $t('firstName') }}</label>
                   <input
-                    v-bind="firstName"
+                    v-model="firstName"
                     type="text"
                     class="form-control"
                     id="firstName"
@@ -90,7 +91,7 @@ pl:
                 <div class="col-md-6">
                   <label for="lastName">{{ $t('lastName') }}</label>
                   <input
-                    v-bind="lastName"
+                    v-model="lastName"
                     type="text"
                     class="form-control"
                     id="lastName"
@@ -100,7 +101,7 @@ pl:
                 <div class="col-md-6">
                   <label for="phoneNumber">{{ $t('number') }}</label>
                   <input
-                    v-bind="phoneNumber"
+                    v-model="phoneNumber"
                     type="text"
                     class="form-control"
                     id="phoneNumber"
@@ -110,7 +111,7 @@ pl:
                 <div class="col-md-6">
                   <label for="email">{{ $t('email') }}</label>
                   <input
-                    v-bind="email"
+                    v-model="email"
                     type="email"
                     class="form-control"
                     id="email"
@@ -120,7 +121,7 @@ pl:
                 <div class="col-md-12">
                   <label for="message">{{ $t('enterMessage') }}</label>
                   <textarea
-                    v-bind="message"
+                    v-model="message"
                     class="form-control"
                     :placeholder="$t('message')"
                     id="message"
@@ -147,9 +148,10 @@ pl:
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
 import Breadcrumb from '~/components/Common/Breadcrumb.vue'
+import Modal from '~/components/Common/Modal.vue'
 
 @Component({
-  components: { Breadcrumb }
+  components: { Breadcrumb, Modal }
 })
 export default class Contact extends Vue {
   phone: string = <string>process.env.phone
@@ -159,6 +161,8 @@ export default class Contact extends Vue {
   companyZip: string = <string>process.env.companyZip
   companyCity: string = <string>process.env.companyCity
   companyMail: string = <string>process.env.companyMail
+  showModal: any = false
+  mMessage: any = 'xsssss'
 
   firstName: string = ''
   lastName: string = ''
@@ -168,23 +172,16 @@ export default class Contact extends Vue {
 
   async sendMessage () {
     try {
-      await this.$axios.post("/contact/mail", {
+      const test = await this.$axios.post("/contact/mail", {
         firstName: this.firstName,
         lastName: this.lastName,
         phoneNumber: this.phoneNumber,
         email: this.email,
         message: this.message
       })
-      this.$root.$emit('popup', {
-        title: 'Dziękujemy za kontakt',
-        message: 'Odpowiemy na tę wiadomość w ciągu paru godzin. Wysłaliśmy również potwierdzenie ' +
-          'wysłania tej wiadomości na twój adres e-mail.',
-      })
+      this.showModal = true
     } catch (err) {
-      this.$root.$emit('popup', {
-        title: 'Przepraszamy',
-        message: 'Nie mogliśmy wysłać wiadomości. Prosimy o kontakt na naszego maila: ' + this.companyMail,
-      })
+      this.showModal = true
     }
 
     return false
