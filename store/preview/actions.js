96e26c1d3a072ab8047822ebb3eee70bc846bb71
aron <vifus1@gmail.com>
2021-12-03 19:11:36 +0100

    fix

--- store/preview/actions.js
+++ store/preview/actions.js
@@ -1,24 +1,6 @@
 import { $axios } from '~/utils/api'
 
 export const getProductPreview = async (apiData, previewId) => {
-  // var apiDataObject = {
-  //   "previewId": previewId,
-  //   "product": {
-  //       "dogIds": [{
-  //           "id": "",
-  //           "variantId": "",
-  //           "name": ""
-  //       }],
-  //       "quoteIds": [],
-  //       "maleHairstyleIds": [],
-  //       "femaleHairstyleIds": [],
-  //       "maleBodyIds": [],
-  //       "femaleBodyIds": [],
-  //       "humanNames": []
-  //   }
-  // }
-  console.log(apiData, 'arle')
-
   return await $axios.post('/preview', apiData)
 }
 
