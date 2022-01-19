4acd035a0fad60f1d3a208519324ffbf3aa90929
Mariusz Buk <mariusz@buk.email>
2022-01-19 23:05:19 +0100

    Fixed loading basket if possible

--- store/basket/actions.ts
+++ store/basket/actions.ts
@@ -5,13 +5,13 @@ import { ProductObject } from '~/store/cup/getters'
 export default {
   addToBasket: async ({ commit }: any, apiData: ProductObject) => {
     const res = await $axios.post('/basket', apiData)
-    commit('setBasket', res.data)
+    commit('setBasket', res.data.basket)
     localStorage.basketToken = res.data.token
   },
 
   editBasket: async ({ commit }: any, apiData: ProductUpdateRequest) => {
     const res = await $axios.patch('/basket', apiData)
-    commit('setBasket', res.data)
+    commit('setBasket', res.data.basket)
   },
 
   loadBasket: async ({ commit }: any) => {
--- store/defaults/actions.ts
+++ store/defaults/actions.ts
@@ -2,7 +2,11 @@ import { $axios } from '~/utils/api'
 
 export default {
   load: async ({ commit }: any) => {
-    const result = await $axios.$get('/defaults')
+    const result = await $axios.$get('/defaults', {
+      headers: {
+        token: localStorage.basketToken
+      }
+    })
     commit('setDefaults', result)
     if (result.basket) {
       commit('basket/setBasket', result.basket, { root: true })
