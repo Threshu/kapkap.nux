176138b51bc96e30874824807875dfb63ae96f66
Paweł Konik <pawel.konik@wenzel-consulting.io>
2022-02-02 13:59:05 +0100

    fix for pr

--- store/categories/actions.ts
+++ store/categories/actions.ts
@@ -4,7 +4,8 @@ import { CategoriesState } from '~/types/categories/types'
 
 export const loadCategories = async ({ commit }: ActionContext<CategoriesState, CategoriesState>) => {
   const res = await $axios.get<CategoriesState>('/categories')
-  if (res.statusText === 'OK') {
+
+  if (res?.data?.success) {
     commit('setStateForCategories', res.data)
   }
 }
