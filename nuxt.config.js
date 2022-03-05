c8e22f1e12cedaa889e5662e5f91c76c6366bc56
Mariusz Buk <mariusz@buk.email>
2022-03-05 09:33:44 +0100

    changed to ssr

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,7 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-  ssr: false,
-  mode: 'client',
+  ssr: true,
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
