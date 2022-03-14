1960631d9ee27affba43f1d1523168549a0dbb1e
Mariusz Buk <mariusz.buk@acx.com.pl>
2022-03-14 23:00:55 +0100

    test static content serving

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'client',
+  target: 'static',
   ssr: true,
 
   // Global page headers: https://go.nuxtjs.dev/config-head
