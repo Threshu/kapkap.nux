00910113dcff0904351a78c52b8166d83f383063
aron <vifus1@gmail.com>
2022-03-14 20:03:59 +0100

    fix

--- store/cup/mutations.ts
+++ store/cup/mutations.ts
@@ -65,27 +65,22 @@ export default {
   },
 
   setQuote (state: EditorState, quoteId: string) {
-
     if (state.workingObject.quoteId === quoteId && !state.editMode) {
       quoteId = ''
     }
 
+    const index = state.workingObject.items.map(function (quote) { return quote.type }).indexOf('quote')
+
     Vue.set(state.workingObject, 'quoteId', quoteId)
     if (state.editMode) {
-      if (state.workingObject.items[1].type === 'quote') {
-        Vue.set(state.workingObject.items[1], 'id', quoteId)
+      if (index !== -1) {
+        Vue.set(state.workingObject.items[index], 'id', quoteId)
       } else {
-        let count = state.workingObject.items.length
-        if (state.workingObject.items[count-1].type==='quote') {
-          Vue.set(state.workingObject.items[count-1], 'id', quoteId)
-        } else {
-          state.workingObject.items.push({
-            'id': quoteId,
-            'type': 'quote'
-          })
-        }
+        state.workingObject.items.push({
+          'id': quoteId,
+          'type': 'quote'
+        })
       }
-
     }
   },
 
